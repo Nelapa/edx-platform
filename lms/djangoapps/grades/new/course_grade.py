b9e18c6825f497e836d672abed4ad1a4f4d8d76c
Eric Fischer <efischer@edx.org>
2016-08-15 14:56:17 -0400

    python test fix?

--- lms/djangoapps/grades/new/course_grade.py
+++ lms/djangoapps/grades/new/course_grade.py
@@ -26,10 +26,8 @@ class CourseGrade(object):
         self.course_structure = course_structure
         self.chapter_grades = []
 
-        # TODO: currently, data model won't accept null for course version, but tests do that a lot. Determine correct solution to this problem.
-            # - course_version not always needed for PersistentSubsectionGrade?
-            # - tests all need to ensure their courses have a course_version?
-        if self.course.course_version is None:
+        # TODO: currently, data model requires a course version, but tests don't always provide one. Determine solution
+        if not hasattr(self.course, 'version') or self.course.course_version is None:
             self.course.course_version = 1
 
     @lazy
